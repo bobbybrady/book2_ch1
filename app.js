46535fa6bd5107754a5e8f45cab3a9b58d2d4879
Bobby Brady <bobbybrady@Bobbys-MacBook-Pro-2.local>
2019-07-12 10:10:26 -0500

    added forEach loop

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const head = document.querySelector(".article__header");
 head.textContent = "Welcome to the {insert your name here} blog";
 
 const important = document.querySelectorAll(".article__header");
-for (let i = 0; i < important.length; i++) {
-    const currentItem = important[i];
-    currentItem.classList.add("important");
-}
+// for (let i = 0; i < important.length; i++) {
+//     const currentItem = important[i];
+//     currentItem.classList.add("important");
+// }
+
+important.forEach((currentNode) => {
+    currentNode.classList.add("important")
+})
     
 console.log(important);
 
